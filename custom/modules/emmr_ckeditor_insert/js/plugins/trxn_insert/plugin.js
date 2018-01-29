fa7222321c6fdc44d63ae9fb236b283b4640ced1
camilocodes <camilo.v@unb.ca>
2018-01-29 13:53:16 -0400

    EMMR-5 Add and register custom CKEditor button

--- custom/modules/emmr_ckeditor_insert/js/plugins/trxn_insert/plugin.js
+++ custom/modules/emmr_ckeditor_insert/js/plugins/trxn_insert/plugin.js
@@ -1,6 +1,14 @@
-CKEDITOR.plugins.add( 'trxn_insert', {
+CKEDITOR.plugins.add('trxn_insert', {
     icons: 'trxn_insert',
-    init: function( editor ) {
-        // Plugin logic goes here...
+
+    init: function(editor) {
+      editor.addCommand('trxn_insert',
+        new CKEDITOR.dialogCommand('trxn_insertDialog'));
+
+      editor.ui.addButton('trxn_insert', {
+        label: 'Transcription Insert',
+        command: 'trxn_insert',
+        toolbar: 'formatting'
+      });
     }
 });
---
+++ custom/modules/emmr_ckeditor_insert/src/Plugin/CKEditorPlugin/TrxnInsert.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\emmr_ckeditor_insert\Plugin\CKEditorPlugin;
+
+use Drupal\ckeditor\CKEditorPluginBase;
+use Drupal\ckeditor\CKEditorPluginCssInterface;
+use Drupal\editor\Entity\Editor;
+
+/**
+ * Defines the "Transcription Insert" plugin.
+ *
+ * @CKEditorPlugin(
+ *   id = "trxn_insert",
+ *   label = @Translation("Transcription Insert"),
+ *   module = "emmr_ckeditor_insert"
+ * )
+ */
+class TrxnInsert extends CKEditorPluginBase implements CKEditorPluginCssInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDependencies(Editor $editor) {
+    return [];
+  }
+
+  /**
+   * Implements \Drupal\ckeditor\Plugin\CKEditorPluginInterface::isInternal().
+   */
+  public function isInternal() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFile() {
+    return drupal_get_path('module', 'emmr_ckeditor_insert') . '/js/plugins/trxn_insert/plugin.js';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getButtons() {
+    return [
+      'trxn_insert' => [
+        'label' => t('Transcription Insert'),
+        'image' => drupal_get_path('module', 'emmr_ckeditor_insert') .
+        '/js/plugins/trxn_insert/icons/trxn_insert.png',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfig(Editor $editor) {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCssFiles(Editor $editor) {
+    return [];
+  }
+
+}
