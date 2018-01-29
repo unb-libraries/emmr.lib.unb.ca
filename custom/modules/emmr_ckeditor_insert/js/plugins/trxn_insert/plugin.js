d7d69765e43096ff4cb296d1292b8365f1fde75f
camilocodes <camilo.v@unb.ca>
2018-01-29 16:07:00 -0400

    EMMR-5 Add transcription-text dialog to CKEditor

--- custom/modules/emmr_ckeditor_insert/js/plugins/trxn_insert/plugin.js
+++ custom/modules/emmr_ckeditor_insert/js/plugins/trxn_insert/plugin.js
@@ -1,14 +1,16 @@
 CKEDITOR.plugins.add('trxn_insert', {
-    icons: 'trxn_insert',
+  icons: 'trxn_insert',
 
-    init: function(editor) {
-      editor.addCommand('trxn_insert',
-        new CKEDITOR.dialogCommand('trxn_insertDialog'));
+  init: function(editor) {
+    editor.addCommand('trxn_insert',
+      new CKEDITOR.dialogCommand('trxn_insertDialog'));
 
-      editor.ui.addButton('trxn_insert', {
-        label: 'Transcription Insert',
-        command: 'trxn_insert',
-        toolbar: 'formatting'
-      });
-    }
+    editor.ui.addButton('trxn_insert', {
+      label: 'Transcription Insert',
+      command: 'trxn_insert',
+      toolbar: 'formatting'
+    });
+
+    CKEDITOR.dialog.add('trxn_insertDialog', this.path + 'dialogs/trxn_insert.js');
+  }
 });
