79b73add616ac7bcbe9fbd3f8fee34d68374848d
camilocodes <camilo.v@unb.ca>
2018-01-31 13:14:43 -0400

    EMMR-5 Implement transcription-insert plugin behavior

--- custom/modules/emmr_ckeditor_insert/js/plugins/trxn_insert/dialogs/trxn_insert.js
+++ custom/modules/emmr_ckeditor_insert/js/plugins/trxn_insert/dialogs/trxn_insert.js
@@ -12,11 +12,21 @@ CKEDITOR.dialog.add('trxn_insertDialog', function(editor) {
           {
           type: 'text',
           id: 'to-insert',
-          label: 'Text to insert',
-          validate: CKEDITOR.dialog.validate.notEmpty( "Text to insert cannot be empty." )
-          }
+          label: 'Text to insert'          }
         ]
       }
-    ]
+    ],
+
+    onOk: function() {
+      var dialog = this;
+      var insertText = dialog.getValueOf('tab-insert', 'to-insert');
+      var insertDiv = editor.document.createElement('div');
+
+      insertDiv.setAttribute('id', 'trxn-insert');
+      insertDiv.setAttribute('class', 'trxn-insert');
+      insertDiv.setText(insertText);
+
+      editor.insertElement(insertDiv);
+    }
   };
 });
