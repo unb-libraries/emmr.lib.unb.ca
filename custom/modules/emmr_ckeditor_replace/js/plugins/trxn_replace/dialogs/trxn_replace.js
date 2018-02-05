afde4416d4178cd426725633e88418b750aab649
camilocodes <camilo.v@unb.ca>
2018-02-05 15:26:33 -0400

    EMMR-5 Create CKEditor module for transcription replacement

---
+++ custom/modules/emmr_ckeditor_replace/js/plugins/trxn_replace/dialogs/trxn_replace.js
@@ -0,0 +1,34 @@
+CKEDITOR.dialog.add('trxn_replaceDialog', function(editor) {
+  return {
+    title: 'Transcription Replace',
+    minWidth: 400,
+    minHeight: 200,
+
+    contents: [
+      {
+        id: 'tab-replace',
+        label: '',
+        elements: [
+          {
+          type: 'text',
+          id: 'to-replace',
+          label: 'Text to replace'          }
+        ]
+      }
+    ],
+
+    onOk: function() {
+      var dialog = this;
+      var replaceText = dialog.getValueOf('tab-replace', 'to-replace');
+      var textSpan = editor.document.createElement('span');
+      var caretSpan = editor.document.createElement('span');
+
+      textSpan.setAttribute('class', 'trxn-text');
+      textSpan.setText(replaceText);
+
+      caretSpan.$.appendChild(textSpan.$);
+
+      editor.replaceElement(caretSpan);
+    }
+  };
+});
