8260d2a27b973cc782350c3acfa4753c03d593f3
Camilo Villamizar <cvillami@unb.ca>
2018-06-27 12:56:32 -0300

    EMMR-18 Complete transcription tool group functionality

--- custom/modules/emmr_ckeditor_undo/js/plugins/trxn_undo/plugin.js
+++ custom/modules/emmr_ckeditor_undo/js/plugins/trxn_undo/plugin.js
@@ -4,7 +4,21 @@ CKEDITOR.plugins.add('trxn_undo', {
   init: function(editor) {
     editor.addCommand('trxn_undo', {
       exec: function(editor) {
-        alert("Hello World!");
+        var selection =
+          editor.getSelectedHtml(true);
+
+        var err = "No transcription content found in selection. " +
+          "Please make sure all elements (red text) are selected, or use the " +
+          "default [Undo] button to reverse formatting step-by-step.";
+
+        if (selection.indexOf("trxn-replace") > 0) {
+          editor.execCommand('undo');
+          editor.execCommand('undo');
+        }
+        else if (selection.indexOf("trxn-caret") > 0) {
+          editor.execCommand('undo');
+        }
+        else alert(err);
       }
     });
 
