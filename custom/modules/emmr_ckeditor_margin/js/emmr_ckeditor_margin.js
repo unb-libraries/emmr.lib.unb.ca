c4c7393736cda1c1a2f2c1f5300e0f178b82eb7c
camilocodes <camilo.v@unb.ca>
2018-07-06 16:26:49 -0300

    EMMR-19 Implement marginalia copying and read pane formatting

--- custom/modules/emmr_ckeditor_margin/js/emmr_ckeditor_margin.js
+++ custom/modules/emmr_ckeditor_margin/js/emmr_ckeditor_margin.js
@@ -1,6 +1,15 @@
 // On document ready.
 jQuery(document).ready(function() {
-  if (jQuery('#recipe-details-header').length != 0) {
-    alert("TEST!!!");
+  if (jQuery("#recipe-details-header").length != 0) {
+    // Recipe node view functionality
+    // Copy annotations from CKEditor to annotations panel
+    origin = ".field--name-field-recipe-transcription .trxn-margin";
+    destin = "#recipe-anno ";
+
+    // If annotations found
+    if ( jQuery(origin).length != 0 ) {
+      jQuery("<p><b>Marginalia</b></p>").appendTo(destin);
+      jQuery(origin).clone().appendTo(destin);
+    }
   }
 });
