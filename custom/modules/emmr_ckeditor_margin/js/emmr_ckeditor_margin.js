c0bbe9601aa0d475225960d0a0a45c9161f8ab7c
Camilo Villamizar <cvillami@unb.ca>
2018-07-09 16:43:29 -0300

    EMMR-19 Create annotations panel when not present + misc

--- custom/modules/emmr_ckeditor_margin/js/emmr_ckeditor_margin.js
+++ custom/modules/emmr_ckeditor_margin/js/emmr_ckeditor_margin.js
@@ -4,10 +4,18 @@ jQuery(document).ready(function() {
     // Recipe node view functionality
     // Copy annotations from CKEditor to annotations panel
     origin = ".field--name-field-recipe-transcription .trxn-margin";
-    destin = "#recipe-anno ";
+    destin = "#recipe-anno";
 
-    // If annotations found
+    // If marginalia found
     if ( jQuery(origin).length != 0 ) {
+      // If annotations panel not found
+      if ( jQuery(destin).length === 0 ) {
+        var anno = "<div class='col-md-2' id='recipe-anno'></div>";
+        var parent = "#pages-trans-anno";
+        jQuery(anno).appendTo(parent);
+      }
+
+      // Append marginalia tittle and items
       jQuery("<p><b>Marginalia</b></p>").appendTo(destin);
       jQuery(origin).clone().appendTo(destin);
     }
