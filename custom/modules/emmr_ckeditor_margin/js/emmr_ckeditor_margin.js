f8f34bbd5bd3e2c97b0e5deec90002f0f3030998
camilocodes <camilo.v@unb.ca>
2018-07-12 15:25:38 -0300

    EMMR-19 Restore marginalia w/ new layout

--- custom/modules/emmr_ckeditor_margin/js/emmr_ckeditor_margin.js
+++ custom/modules/emmr_ckeditor_margin/js/emmr_ckeditor_margin.js
@@ -1,15 +1,27 @@
 // On document ready.
 jQuery(document).ready(function() {
-  // If node view page
   if (jQuery("#recipe-details-header").length != 0) {
     // Recipe node view functionality
+    // Copy annotations from CKEditor to annotations panel
+    origin = ".field--name-field-recipe-transcription .trxn-margin";
+    destin = "#recipe-anno";
 
-    // If annotations panel present
-    if (jQuery("#recipe-anno").length != 0) {
-      // If marginalia present
-      if (jQuery(".trxn-margin").length != 0) {
-
+    // If marginalia found
+    var margins = jQuery(origin).clone();
+    jQuery("<br><br>").appendTo(margins);
+    if ( jQuery(origin).length != 0 ) {
+      // If annotations panel not found
+      if ( jQuery(destin).length === 0 ) {
+        var anno = "<div class='col-md-2' id='recipe-anno'></div>";
+        var parent = "#pages-trans-anno";
+        jQuery(anno).appendTo(parent);
       }
+
+      // Append marginalia tittle and items
+      jQuery("<br><p><b>Marginalia</b></p>").appendTo(destin);
+      jQuery("<div id='marginalia'></div>").appendTo(destin);
+      destin += " > div#marginalia";
+      jQuery(margins).clone().appendTo(destin);
     }
   }
 });
