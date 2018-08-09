ee2493a28ce7ceb0efda716b4e93d1443efab3b7
camilocodes <camilo.v@unb.ca>
2018-08-09 16:04:17 -0300

    EMMR-14 Create direct route to PDF generation - not working

--- custom/modules/emmr_core/src/Controller/EmmrCoreController.php
+++ custom/modules/emmr_core/src/Controller/EmmrCoreController.php
@@ -3,8 +3,8 @@
 namespace Drupal\emmr_core\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Routing\TrustedRedirectResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Dompdf\Dompdf;
 
 /**
  * Controller for Datasets navigation.
@@ -20,6 +20,31 @@ class EmmrCoreController extends ControllerBase {
       '#attributes' => [],
     ];
     return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function pdf() {
+    // Instantiate and use the dompdf class.
+    $dompdf = new Dompdf();
+
+    // Render node view html to string.
+    $node = \Drupal::entityManager()->getStorage('node')->load(2);
+    $view_builder = \Drupal::entityManager()->getViewBuilder('node');
+    $renderarray = $view_builder->view($node, 'full');
+    $html = \Drupal::service('renderer')->renderRoot($renderarray);
+
+    // Load html into dompdf.
+    $dompdf->loadHtml($html, "utf-8");
+
+    // Render the HTML as PDF.
+    $dompdf->render();
+
+    // Output the generated PDF to Browser.
+    kint($html);
+    $dompdf->stream();
+    exit;
   }
 
   /**
