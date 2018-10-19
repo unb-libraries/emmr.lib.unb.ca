d686bf76298e3455cdd3ed5b050f80b74e8616c3
camilocodes <camilo.v@unb.ca>
2018-10-19 11:04:27 -0300

    EMMR-58 Add utility script to refresh db

---
+++ custom/modules/emmr_util/refresh_recipes.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains refresh_recipes.php.
+ */
+
+use Drupal\node\Entity\Node;
+
+// Get an array of all 'recipe' node ids.
+$article_nids = \Drupal::entityQuery('node')
+  ->condition('type', 'emmr_recipe')
+  ->execute();
+
+// Load all the articles.
+$articles = Node::loadMultiple($article_nids);
+foreach ($articles as $article) {
+  $article->save();
+}
