e35ed1ea8ab1b3b0566e5624f4c58ba000e76e29
Camilo Villamizar <cvillami@unb.ca>
2023-11-09 12:46:47 -0400

    EMMR-107 Remove old plugins, cleanup emmr_transcribe, config

---
+++ custom/modules/emmr_transcribe/js/ckeditor5_plugins/transInsert/src/inserttransinsertcommand.js
@@ -0,0 +1,54 @@
+/**
+ * @file defines InsertSimpleBoxCommand, which is executed when the simpleBox
+ * toolbar button is pressed.
+ */
+// cSpell:ignore transinsertediting
+
+import { Command } from 'ckeditor5/src/core';
+
+export default class InsertTransInsertCommand extends Command {
+  execute() {
+    const { model } = this.editor;
+
+    model.change((writer) => {
+      // Insert <simpleBox>*</simpleBox> at the current selection position
+      // in a way that will result in creating a valid model structure.
+      model.insertContent(createTransInsert(writer));
+    });
+  }
+
+  refresh() {
+    const { model } = this.editor;
+    const { selection } = model.document;
+
+    // Determine if the cursor (selection) is in a position where adding a
+    // simpleBox is permitted. This is based on the schema of the model(s)
+    // currently containing the cursor.
+    const allowedIn = model.schema.findAllowedParent(
+      selection.getFirstPosition(),
+      'transInsert',
+    );
+
+    // If the cursor is not in a location where a simpleBox can be added, return
+    // null so the addition doesn't happen.
+    this.isEnabled = allowedIn !== null;
+  }
+}
+
+function createTransInsert(writer) {
+  // Create instances of the three elements registered with the editor in
+  // transinsertediting.js.
+  const transInsert = writer.createElement('transInsert');
+  const transInsertCaret = writer.createElement('transInsertCaret');
+  writer.appendText('^', {}, transInsertCaret);
+  const transInsertText = writer.createElement('transInsertText');
+  writer.appendText('Type transcription insert text here', {}, transInsertText);
+
+  // Append the title and description elements to the simpleBox, which matches
+  // the parent/child relationship as defined in their schemas.
+  writer.append(transInsertCaret, transInsert);
+  writer.append(transInsertText, transInsert);
+
+  // Return the element to be added to the editor.
+  return transInsert;
+}
