8d08577ce2f819a71dfd914067d60ca804d48d8c
Camilo Villamizar <cvillami@unb.ca>
2023-12-14 19:17:42 -0400

    EMMR-107 Update text format update script

--- custom/modules/emmr_util/libtext.php
+++ custom/modules/emmr_util/libtext.php
@@ -7,25 +7,81 @@
  * Set all node text fields to use unb_libraries text format.
  */
 
-// Search for all anniversary node ids.
+echo "\nStarting unb_libraries text format override...\n\n";
+
+// Search for all article node ids.
+$nids = Drupal::entityQuery('node')
+  ->condition('type', 'article')->accessCheck(FALSE)->execute();
+
+// Load and update all articles.
+foreach ($nids as $nid) {
+  $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+  $node->body->format = 'unb_libraries_basic';
+  $node->save();
+  $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+  $title = $node->title->value;
+
+  if ($node->body->format != 'unb_libraries_basic') {
+   echo "Could not update body for article [$title]\n"; 
+  }
+  else {
+    echo "Updated body for article [$title]\n";
+  }
+}
+
+// Search for all page node ids.
+$nids = Drupal::entityQuery('node')
+  ->condition('type', 'page')->accessCheck(FALSE)->execute();
+
+// Load and update all pages.
+foreach ($nids as $nid) {
+  $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+  $node->body->format = 'unb_libraries_basic';
+  $node->save();
+  $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+  $title = $node->title->value;
+
+  if ($node->body->format != 'unb_libraries_basic') {
+   echo "Could not update body for page [$title]\n"; 
+  }
+  else {
+    echo "Updated body for page [$title]\n";
+  }
+}
+
+// Search for all recipe node ids.
 $nids = Drupal::entityQuery('node')
   ->condition('type', 'emmr_recipe')->accessCheck(FALSE)->execute();
 
-echo "\nStarting unb_libraries text format override...\n\n";
-
-// Load and update all anniversaries.
+// Load and update all recipes.
 foreach ($nids as $nid) {
   $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
   $node->field_abstract->format = 'unb_libraries_basic';
+  $node->field_ingredients_index->format = 'unb_libraries_basic';
+  $node->field_symptoms_index->format = 'unb_libraries_basic';
   $node->save();
   $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
   $title = $node->title->value;
 
   if ($node->field_abstract->format != 'unb_libraries_basic') {
-   echo "Could not update field_abstract for [$title]\n"; 
+   echo "Could not update field_abstract for recipe [$title]\n"; 
   }
   else {
-    echo "Updated field_abstract for [$title]\n";
+    echo "Updated field_abstract for recipe [$title]\n";
+  }
+
+  if ($node->field_ingredients_index->format != 'unb_libraries_basic') {
+   echo "Could not update field_ingredients_index for recipe [$title]\n"; 
+  }
+  else {
+    echo "Updated field_ingredients_index for recipe [$title]\n";
+  }
+
+  if ($node->field_symptoms_index->format != 'unb_libraries_basic') {
+   echo "Could not update field_symptoms_index for recipe [$title]\n"; 
+  }
+  else {
+    echo "Updated field_symptoms_index for recipe [$title]\n";
   }
 }
 
